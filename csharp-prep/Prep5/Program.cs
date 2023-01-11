045903c968ad31cae22a7bc94af75155f0aef9f9
MagnesiumSail <Leapster16@gmail.com>
2023-01-11 13:47:56 -0700

    Finished Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int favNumber)
+    {
+        int output = favNumber * favNumber;
+        return output;
+    }
+    static void DisplayResult(string name, int number )
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayMessage();
+
+        string nameOut = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int SquaredNumber = SquareNumber(favNumber);
+
+        DisplayResult(nameOut, SquaredNumber);
     }
 }
